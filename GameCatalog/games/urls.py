bb8feb5f78017cffd774a8352ef3925fc3b9e2e9
zhaoyang89-commits <zhuxiaopeng89@gmail.com>
2026-06-11 05:56:19 +0800

    Delete GameCatalog directory

--- GameCatalog/games/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('games/', views.game_list, name='game_list'),
-    path('games/<int:game_id>/', views.game_detail, name='game_detail'),
-    path('games/add/', views.add_game, name='add_game')
-]

640870e5a48d9fea6b8b9a0d77658a99bfd4bf25
zhaoyang89-commits <zhuxiaopeng89@gmail.com>
2026-06-13 03:02:10 +0800

    Update settings.py

--- GameCatalog/GameCatalog/settings.py
+++ GameCatalog/GameCatalog/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
